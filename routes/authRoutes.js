253e5624046c8fdcbea0cb27ceaf410b2821f3e3
Haris Sabanovic <hasa2303@student.miun.se>
2024-08-26 13:25:19 +0200

    hashar admin lösenord o sen kollar ifall det är korrekt användarnamn o lösenord

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,18 +2,40 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+require("dotenv").config();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 //connect to mongodb
 mongoose.set("strictQuery", false);
-mongoose.connect(process.env.DATABASE).then(() => {
+mongoose.connect("mongodb://127.0.0.1:27017/resteraunt").then(() => { 
     console.log("Connected to MongoDB");
 }).catch((error) => {
     console.log(error + " Error connecting to database")
 })
 
+// Hårdkodat användarnamn och lösenord
+const adminUsername = process.env.ADMINNAME;
+const adminPassword = bcrypt.hashSync(process.env.PASSWORD, 10);
+
+
+
 //loggar in användare
 router.post("/login", async (req, res) => {
-    console.log("Sup")
+    const { username, password } = req.body;
+
+  // Kontrollera användarnamnet
+  if (username !== adminUsername) {
+    return res.status(400).json({ message: 'Fel användarnamn' });
+  }
+
+  // Kontrollera lösenordet
+  const isMatch = await bcrypt.compare(password, adminPassword);
+  if (!isMatch) {
+    return res.status(400).json({ message: 'Fel lösenord' });
+  } else {
+    return res.json({message: "Inloggad"})
+  }
 })
 
 module.exports = router;
