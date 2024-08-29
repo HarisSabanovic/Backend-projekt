4568d349faff3e73efd463e3889c30c5a305dc29
Haris Sabanovic <hasa2303@student.miun.se>
2024-08-29 13:03:39 +0200

    rutt som autentiserar token

--- server.js
+++ server.js
@@ -16,6 +16,30 @@ app.use(cors());
 //routes
 app.use("/api", authRoutes);
 
+//protected route
+app.get("/api/protected", authenticateToken, (req, res) => {
+    res.json({ message: "Inloggad på skyddad server"});
+})
+
+//validate token
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if(token == null) {
+        res.status(401).json({ message: "not authorized"});
+    }
+
+    jwt.verify(token, process.env.JWT_KEY, (err, username) => {
+        if(err) {
+            return res.status(403).json({ message: "invalid jwt"});
+        }
+
+        req.username = username;
+        next();
+    });
+}
+
 app.listen(port, () => {
     console.log(`Servern är igång på localhost:${port}`)
 } )
