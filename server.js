29c8db6fa80343c7e8d1a793a7337d0daf84b10f
Haris Sabanovic <hasa2303@student.miun.se>
2024-09-02 17:52:26 +0200

    port ändring

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const jwt = require("jsonwebtoken");
 const authRoutes = require("./routes/authRoutes");
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(cors());
